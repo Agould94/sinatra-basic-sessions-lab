3ee3fae6b36061c4af625bb18b63471d99a407ba
Alexander Gould <alexander.gould94@gmail.com>
2021-05-06 14:27:46 -0700

    commit tree 639d22e423376d4ea7a9ff39d0c928942ddfc9ec

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    set :views, settings.root + '/5:3-5:7/sinatra-basic-sessions-lab/views'
+    configure do
+        enable :sessions
+        set :session_secret, "secret"
+    end
+    get '/' do
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = params[:item]
+        @session = session
+        erb :checkout
+    end
 end
